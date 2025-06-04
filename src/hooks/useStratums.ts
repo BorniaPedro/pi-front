6ba086ac88ef5636c2f64a70282103600a956634
viictor1 <victorgaspalmeida@hotmail.com>
2025-06-04 16:46:25 -0300

    core: adicinando sweetalert

--- src/hooks/useStratums.ts
+++ src/hooks/useStratums.ts
@@ -2,6 +2,7 @@
 // hooks/useGetStratums.ts
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { StratumForm, Stratum } from '@/lib/stratumSchema';
+import { alertError, alertSuccess } from '@/lib/alert';
 
 const STRATUMS_QUERY_KEY = 'stratums';
 
@@ -46,7 +47,7 @@ export function useCreateStratum() {
             queryClient.invalidateQueries({ queryKey: [STRATUMS_QUERY_KEY] });
         },
         onError: (error) => {
-            alert(error.message);
+            alertError(error.message);
         },
     })
 }
@@ -72,10 +73,10 @@ export function useUpdateStratum() {
         onSuccess: (updatedStratum) => {
             queryClient.invalidateQueries({ queryKey: [STRATUMS_QUERY_KEY, updatedStratum.id] });
             queryClient.invalidateQueries({ queryKey: [STRATUMS_QUERY_KEY] });
-            alert('Stratum atualizado com sucesso!');
+            alertSuccess('Stratum atualizado com sucesso!');
         },
         onError: (error: Error) => {
-            alert(error.message);
+            alertError(error.message);
         },
     });
 }
@@ -97,10 +98,10 @@ export function useDeleteStratum() {
         },
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['stratums'] });
-            alert('Stratum deletado com sucesso!');
+            alertSuccess('Stratum deletado com sucesso!');
         },
         onError: (error: Error) => {
-            alert(error.message);
+            alertError(error.message);
         },
     });
 }
